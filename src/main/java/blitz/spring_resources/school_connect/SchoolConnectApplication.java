c0250063d91837aeea7a1f2d1a4ccce2c8fc0bae
Abhinab Dutta Gupta <abhinab.duttagupta@gmail.com>
2025-09-15 06:01:59 +0530

    initial-commit

---
+++ src/main/java/blitz/spring_resources/school_connect/SchoolConnectApplication.java
@@ -0,0 +1,13 @@
+package blitz.spring_resources.school_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blitz/spring_resources/school_connect/SchoolConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package blitz.spring_resources.school_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
